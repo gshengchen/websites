68fc7a31d2ca860fb304b53c0b83a66277148b92
gshengchen <gsheng.chen@gmail.com>
2019-08-26 18:21:26 +0800

    modify

---
+++ clang/cprog/chapter01/exp1_13.c
@@ -0,0 +1,18 @@
+/**
+ * 练习1-13 打印输入单词中的单词长度的直方图
+ */
+
+#include <stdio.h>
+#define IN 1
+#define OUT 0
+
+void main()
+{
+    int c, state;
+    state = OUT;
+    while ((c = getchar()) != EOF)
+    {
+        
+    }
+
+}
