63748b5bd301da8bde2ac7055301bff3a864c231
gshengchen <gsheng.chen@gmail.com>
2021-03-16 20:25:31 +0800

    add files

--- com/breoe/thinking/ch03/Equivalence.java
+++ com/breoe/thinking/ch03/Equivalence.java
@@ -5,12 +5,14 @@ package com.breoe.thinking.ch03;
 
 public class Equivalence {
     public static void main(String[] args){
-        Integer n1 =new Integer(47);
-        Integer n2 = new Integer(47);
+       // Integer n1 =new Integer(47);
+        //Integer n2 = new Integer(47);
+        int n1 = 47;
+        int n2 = 47;
 
         System.out.println(n1 == n2);
         System.out.println(n1 != n2);
-        System.out.println(n1.equals(n2));
+//        System.out.println(n1.equals(n2));
     }
     
 }
---
+++ com/breoe/thinking/ch04/Flower.java
@@ -0,0 +1,46 @@
+/**
+ * this如果有参数就是都符合参数的构造函数进行调用,
+ * 只能用在构造函数中并只能用一次
+ */
+package com.breoe.thinking.ch04;
+
+public class Flower {
+    private int petalCount = 0;
+    private String s = new String("null");
+
+    Flower(int petals){
+        petalCount = petals;
+        System.out.println("constructor w/ intarg only, petalCount = " + petalCount);
+
+    }
+
+    Flower(String ss){
+        s = ss;
+        System.out.println("constructor w/ String only, s = " + s);
+    }     
+
+    Flower(String s, int petals){
+        this(petals);
+        //! this(s);
+        this.s = s;
+        System.out.println("constructor  String && int ");
+    }
+
+    Flower(){
+        this("hi",47);
+        System.out.println("default construct no arg");
+    }
+
+    void print(){
+        //! this(11);
+       System.out.println("petalCount = " + petalCount 
+                           + " s = " + s);
+    }
+
+    public static void main(String[] args){
+        Flower x = new Flower();
+        x.print();
+    } 
+     
+    
+}
