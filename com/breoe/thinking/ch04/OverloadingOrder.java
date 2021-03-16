dc3debd0b05ab7bbf70648258ae10a205659c35a
gshengchen <gsheng.chen@gmail.com>
2021-03-16 16:18:11 +0800

    add files

---
+++ com/breoe/thinking/ch04/OverloadingOrder.java
@@ -0,0 +1,20 @@
+/**
+ * 自变量顺序也能重载
+ */
+package com.breoe.thinking.ch04;
+
+public class OverloadingOrder {
+    static void print(String s, int i){
+        System.out.println("String: " + s + ", int: " + i);
+    }
+
+    static void print(int i, String s){
+
+        System.out.println("int: " + i + ",String: " + s);
+    }
+   
+    public static void main(String[] args){
+        print("String first", 11);
+        print(99, "int first");
+    }
+}
---
+++ com/breoe/thinking/ch04/SimpleConstructor.java
@@ -0,0 +1,17 @@
+/**
+ * 简单的构造器
+ */
+package com.breoe.thinking.ch04;
+
+class Rock{
+    Rock(){
+        System.out.println("creating Rock!");
+    }
+}
+public class SimpleConstructor {
+    public static void main(String[] args){
+        for(int i = 0; i < 10; i++)
+           new Rock();
+    }
+    
+}
